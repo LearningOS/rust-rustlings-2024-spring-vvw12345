b9f8853b15fd4dbef23fc8d5ae920892f6d1d148
yzw12138~ <112001227+vvw12345@users.noreply.github.com>
2024-04-12 06:45:36 +0000

    update

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -3,14 +3,18 @@
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-fn main() {
-    my_macro!();
-}
 
+//宏的定义要在使用之前
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
 }
+
+fn main() {
+    my_macro!();
+}
+
+
+
